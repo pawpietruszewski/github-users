c8706c60531f2bb1e78b06e4d48c62d073f10fcf
pawpietruszewski <pawpietruszewski@gmail.com>
2022-09-25 13:46:51 +0200

    Add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,28 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 import Layout from 'src/components/Layout';
 import HomePage from 'src/components/HomePage';
 
+import {
+  META_DESCRIPTION,
+  META_TITLE,
+} from 'src/constants/meta';
+
 export const Page: NextPage = (): JSX.Element => {
   return (
-    <Layout >
-      <HomePage />
-    </Layout>
+    <>
+      <Head>
+        <title key="title">{META_TITLE}</title>
+        <meta property="og:title" content={META_TITLE} key="ogTitle" />
+        <meta name="description" content={META_DESCRIPTION} key="description" />
+        <meta property="og:description" content={META_DESCRIPTION} key="ogDescription" />
+        <meta key="twitterTitle" name="twitter:title" content={META_TITLE} />
+        <meta key="twitterDescription" name="twitter:description" content={META_DESCRIPTION} />
+      </Head>
+      <Layout >
+        <HomePage />
+      </Layout>
+    </>
   );
 };
 
---
+++ src/constants/meta.ts
@@ -0,0 +1,2 @@
+export const META_TITLE = 'Github users';
+export const META_DESCRIPTION = 'Application allows you to search for github users';
